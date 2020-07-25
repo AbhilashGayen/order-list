d5763e182ce8f5647ddd93d9f16443e01f190a1d
Abhilash Gayen <abhilashgayen@Abhilashs-MacBook-Pro.local>
2020-07-25 15:42:47 +0530

    Added Layout Component

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,16 +7,25 @@ import Header from "./header/header";
 import styled from "@emotion/styled";
 
 const LayoutContainer = styled.div`
-  margin: 0;
+  width: 90%;
+  margin: 0 auto;
   padding: 0;
 `;
 
+//Interface
+interface Props {
+  children: any;
+}
+
 //Page Layout
-const Layout = () => {
+const Layout = ({ children }: Props) => {
   return (
-    <LayoutContainer>
+    <>
       <Header />
-    </LayoutContainer>
+      <LayoutContainer>
+        <div>{children}</div>
+      </LayoutContainer>
+    </>
   );
 };
 
