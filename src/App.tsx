e23447f77dbd1fc3d2bac9196b500126af16436f
Abhilash Gayen <abhilashgayen@Abhilashs-MacBook-Pro.local>
2020-07-25 17:00:51 +0530

    Added header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-import OrderPage from "./pages/order";
 import { Switch, Route } from "react-router-dom";
+
+//Components
+import Header from "./components/header/header";
+
+//Pages
+import OrderPage from "./pages/order";
 
 function App() {
   return (
     <>
+      <Header />
       <Switch>
         <Route exact path="/orders" component={OrderPage} />
       </Switch>
